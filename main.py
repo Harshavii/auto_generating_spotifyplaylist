4d833c5b15db056558a6fa75e33b6bcd35992c24
Harshavii <112164150+Harshavii@users.noreply.github.com>
2023-02-12 12:32:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ index = 1
 #spotify authentication
 from spotipy.oauth2 import SpotifyOAuth
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="ac1b76c8af0749bfae0287ea1c046fc0",
-                                               client_secret="dfad15e947314a11a66df4dc86860b20",
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="CLIENT_ID",
+                                               client_secret="CLIENT_SECRET",
                                                redirect_uri="https://example.com",
                                                scope="playlist-modify-private",
                                                show_dialog=True,
